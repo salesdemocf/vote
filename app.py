281720e1b67f1754dba684b1e2498c2dda3bc456
Dustin Van Buskirk <dvb@octopus.com>
2025-02-20 14:25:24 -0800

    change vote a to heroes

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import random
 import json
 import logging
 
-option_a = os.getenv('OPTION_A', "Wizards")
+option_a = os.getenv('OPTION_A', "Heroes")
 option_b = os.getenv('OPTION_B', "Trolls")
 hostname = socket.gethostname()
 

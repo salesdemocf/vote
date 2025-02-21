f94f4f77350d453ccc45334466171a163680766b
Dustin Van Buskirk <dvb@octopus.com>
2025-02-21 07:48:38 -0800

    change vote a to warriors

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import random
 import json
 import logging
 
-option_a = os.getenv('OPTION_A', "Elves")
+option_a = os.getenv('OPTION_A', "Warriors")
 option_b = os.getenv('OPTION_B', "Minions")
 hostname = socket.gethostname()
 
